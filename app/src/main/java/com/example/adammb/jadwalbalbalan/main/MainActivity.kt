65d36c78f0a9386a83df46fdb47928f84ad2762a
Adam Mukharil Bachtiar <adammbachtiar@gmail.com>
2018-10-19 11:08:28 +0700

    Periksa tahap 1

--- app/src/main/java/com/example/adammb/jadwalbalbalan/main/MainActivity.kt
+++ app/src/main/java/com/example/adammb/jadwalbalbalan/main/MainActivity.kt
@@ -36,7 +36,7 @@ class MainActivity : AppCompatActivity() {
                 replaceFragment(EventFragment.newInstance(EventFragment.EVENT_TYPE_NEXT))
                 return@OnNavigationItemSelectedListener true
             }
-            navigation_favorite -> {
+            R.id.navigation_favorite -> {
                 supportActionBar?.title = getString(R.string.title_favorite)
                 replaceFragment(EventFragment.newInstance(EventFragment.EVENT_TYPE_FAVORITE))
                 return@OnNavigationItemSelectedListener true
