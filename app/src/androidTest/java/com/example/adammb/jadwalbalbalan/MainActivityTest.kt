8694f7babda71719ae49de6a5dc30df8ac20f2dd
Adam Mukharil Bachtiar <adammbachtiar@gmail.com>
2018-10-29 08:45:44 +0700

    Update unit testing for MainActivity

--- app/src/androidTest/java/com/example/adammb/jadwalbalbalan/MainActivityTest.kt
+++ app/src/androidTest/java/com/example/adammb/jadwalbalbalan/MainActivityTest.kt
@@ -5,12 +5,19 @@ import android.support.test.espresso.Espresso.pressBack
 import android.support.test.espresso.action.ViewActions.click
 import android.support.test.espresso.assertion.ViewAssertions.matches
 import android.support.test.espresso.contrib.RecyclerViewActions
+import android.support.test.espresso.matcher.ViewMatchers
 import android.support.test.espresso.matcher.ViewMatchers.isDisplayed
 import android.support.test.espresso.matcher.ViewMatchers.withId
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
 import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.view.ViewGroup
 import com.example.adammb.jadwalbalbalan.main.MainActivity
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers
+import org.hamcrest.TypeSafeMatcher
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -23,17 +30,17 @@ class MainActivityTest {
 
     @Test
     fun testAppBehaviour() {
-        val bottomNavigationViewNextMatch = onView(withId(R.id.navigation_next_match))
-        bottomNavigationViewNextMatch.check(matches(isDisplayed()))
-        bottomNavigationViewNextMatch.perform(click())
+        val bottomNavigationViewTeam = onView(withId(R.id.navigation_team))
+        bottomNavigationViewTeam.check(matches(isDisplayed()))
+        bottomNavigationViewTeam.perform(click())
 
         Thread.sleep(2000)
-        val recyclerViewNextMatch = onView(withId(R.id.event_recyclerview))
-        recyclerViewNextMatch
+        val recyclerViewTeam = onView(withId(R.id.team_recyclerview))
+        recyclerViewTeam
                 .check(matches(isDisplayed()))
-        recyclerViewNextMatch
+        recyclerViewTeam
                 .perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(12))
-        recyclerViewNextMatch
+        recyclerViewTeam
                 .perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(9, click()))
 
         val actionMenuItemView = onView(withId(R.id.menu_favorite))
@@ -46,5 +53,31 @@ class MainActivityTest {
         val bottomNavigationViewFavorite = onView(withId(R.id.navigation_favorite))
         bottomNavigationViewFavorite.check(matches(isDisplayed()))
         bottomNavigationViewFavorite.perform(click())
+
+        val tabFavoriteTab = onView(Matchers.allOf(ViewMatchers.withContentDescription("Team"),
+                childAtPosition(
+                        childAtPosition(
+                                withId(R.id.tabFavoriteTab),
+                                0),
+                        1),
+                isDisplayed()))
+        tabFavoriteTab.perform(click())
+    }
+
+    private fun childAtPosition(
+            parentMatcher: Matcher<View>, position: Int): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && parentMatcher.matches(parent)
+                        && view == parent.getChildAt(position)
+            }
+        }
     }
 }
