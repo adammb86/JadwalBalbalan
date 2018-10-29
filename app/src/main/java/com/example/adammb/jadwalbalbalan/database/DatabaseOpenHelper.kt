d7ed5f8e098243c3d41043e344609b6cb0b3ca79
Adam Mukharil Bachtiar <adammbachtiar@gmail.com>
2018-10-29 08:07:49 +0700

    Update DatabaseOpenHelper

--- app/src/main/java/com/example/adammb/jadwalbalbalan/database/DatabaseOpenHelper.kt
+++ app/src/main/java/com/example/adammb/jadwalbalbalan/database/DatabaseOpenHelper.kt
@@ -2,10 +2,10 @@ package com.example.adammb.jadwalbalbalan.database
 
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
-import android.os.Build.ID
+import android.provider.BaseColumns
 import android.provider.CalendarContract.Instances.EVENT_ID
+import com.example.adammb.jadwalbalbalan.model.event.Event
 import com.example.adammb.jadwalbalbalan.model.event.Event.Favorite.DATE
-import com.example.adammb.jadwalbalbalan.model.event.Event.Favorite.TABLE_FAVORITE
 import com.example.adammb.jadwalbalbalan.model.event.Event.Favorite.TEAM_AWAY_GOAL_DETAILS
 import com.example.adammb.jadwalbalbalan.model.event.Event.Favorite.TEAM_AWAY_ID
 import com.example.adammb.jadwalbalbalan.model.event.Event.Favorite.TEAM_AWAY_LINEUP_DEFENSE
@@ -24,9 +24,10 @@ import com.example.adammb.jadwalbalbalan.model.event.Event.Favorite.TEAM_HOME_LI
 import com.example.adammb.jadwalbalbalan.model.event.Event.Favorite.TEAM_HOME_NAME
 import com.example.adammb.jadwalbalbalan.model.event.Event.Favorite.TEAM_HOME_SCORE
 import com.example.adammb.jadwalbalbalan.model.event.Event.Favorite.TEAM_HOME_SHOTS
+import com.example.adammb.jadwalbalbalan.model.team.Team
 import org.jetbrains.anko.db.*
 
-class DatabaseOpenHelper(context: Context) : ManagedSQLiteOpenHelper(context, "db_favorite_match.db", null, 1) {
+class DatabaseOpenHelper(context: Context) : ManagedSQLiteOpenHelper(context, "db_jadwalbalbalan.db", null, 1) {
     companion object {
         private var instance: DatabaseOpenHelper? = null
 
@@ -41,10 +42,11 @@ class DatabaseOpenHelper(context: Context) : ManagedSQLiteOpenHelper(context, "d
     }
 
     override fun onCreate(db: SQLiteDatabase) {
-        db.createTable(TABLE_FAVORITE, true,
-                ID to INTEGER + PRIMARY_KEY + AUTOINCREMENT,
+        db.createTable(Event.TABEL_FAVORITE_MATCH, true,
+                BaseColumns._ID to INTEGER + PRIMARY_KEY + AUTOINCREMENT,
                 EVENT_ID to TEXT,
                 DATE to TEXT,
+                Event.TIME to TEXT,
                 TEAM_HOME_ID to TEXT,
                 TEAM_HOME_NAME to TEXT,
                 TEAM_HOME_SCORE to TEXT,
@@ -62,11 +64,23 @@ class DatabaseOpenHelper(context: Context) : ManagedSQLiteOpenHelper(context, "d
                 TEAM_AWAY_LINEUP_GOALKEEPER to TEXT,
                 TEAM_AWAY_LINEUP_DEFENSE to TEXT,
                 TEAM_AWAY_LINEUP_MIDFIELD to TEXT,
-                TEAM_AWAY_LINEUP_FORWARD to TEXT)
+                TEAM_AWAY_LINEUP_FORWARD to TEXT,
+                Event.SPORT_TYPE to TEXT)
+
+
+        db.createTable(Team.TABEL_FAVORITE_TEAM, true,
+                BaseColumns._ID to INTEGER + PRIMARY_KEY + AUTOINCREMENT,
+                Team.TEAM_ID to TEXT,
+                Team.TEAM_NAME to TEXT,
+                Team.TEAM_BADGE to TEXT,
+                Team.TEAM_STADIUM to TEXT,
+                Team.TEAM_FORMED_YEAR to TEXT,
+                Team.TEAM_DESCRIPTION to TEXT)
     }
 
     override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
-        db.dropTable(TABLE_FAVORITE, true)
+        db.dropTable(Event.TABEL_FAVORITE_MATCH, true)
+        db.dropTable(Team.TABEL_FAVORITE_TEAM, true)
     }
 }
 
